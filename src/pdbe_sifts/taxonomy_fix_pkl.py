12dacfad6d59e5d7ff1de905e44c76cdaf322bfd
adamb <adamb@hl-codon-26-02.ebi.ac.uk>
2026-02-26 16:09:21 +0000

    gzip for tax file

--- src/pdbe_sifts/taxonomy_fix_pkl.py
+++ src/pdbe_sifts/taxonomy_fix_pkl.py
@@ -2,6 +2,7 @@
 
 import os
 import pickle as pickle
+import gzip
 from collections.abc import Mapping
 from importlib.resources import files
 
@@ -9,11 +10,11 @@ class TaxonomyFix:
     _dictionary: Mapping[str, Mapping[str, str]] = {}
 
     def __init__(self, conn=None, pkl_file=None):
-        pkl_file = files("pdbe_sifts.data").joinpath("nf90_taxid.pkl")
+        pkl_file = files("pdbe_sifts.data").joinpath("nf90_taxid.pkl.gz")
         self._dictionary = {}
 
         if os.path.isfile(pkl_file):
-            with open(pkl_file, "rb") as f:
+            with gzip.open(pkl_file, "rb") as f:
                 self._dictionary = pickle.load(f)
 
     def get(self, entry, entity):
